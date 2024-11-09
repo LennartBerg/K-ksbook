9d980b19055db2f7092cb13a2d35104696c9cd86
Lennart Berg <lennart.berg@uni-oldenburg.de>
2024-11-09 20:45:07 +0100

    Add changes here and there

--- LennartBerg6/deleteUser.js
+++
@@ -1,3 +0,0 @@
-function confirmDeletion() {
-    return confirm("Are you sure you want to delete your account? All your recipes, comments and reviews will be deleted. This action cannot be undone.");
-}
